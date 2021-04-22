d1ffdd60a18d8e34ff3ac0489c7b2a8d87eda682
Agata0984 <58346439+Agata0984@users.noreply.github.com>
2021-04-22 13:44:03 +0200

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Slownik<string, int> s=new Slownik<string, int>();
+            s.Add("po", 5);
+            Console.WriteLine( s.Take("po"));
+            s.Del("po");
+            s.Add("Ala", 3);
+            s.Add("ma", 1);
+            s.Add("kota", 4);
+            s.Add("psa", 415);
+            Console.WriteLine( s.Take("Ala"));
+            Console.WriteLine( s.Take("ma"));
+            Console.WriteLine( s.Take("kota"));
+            Console.WriteLine( s.Take("Ala"));
+            Console.WriteLine( s.Take("ma"));
+            Console.WriteLine( s.Take("psa"));
+            s.Del("psa");
+            Console.WriteLine( s.Take("Ala"));
+            Console.WriteLine( s.Take("ma"));
+            Console.WriteLine( s.Take("psa"));
+
+        }
+    }
+}
